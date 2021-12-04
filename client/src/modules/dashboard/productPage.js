dcb90c8f14d2023209993874e0d15103caa00c1b
Harsh Bhatt <harshb669@gmail.com>
2021-12-04 19:04:30 -0400

    node modules deleted and minor changes in frontend

--- client/src/modules/dashboard/productPage.js
+++ client/src/modules/dashboard/productPage.js
@@ -1,69 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { loadIntoDatabase, fetchSentiment } from '../../service';
-import axios from 'axios';
+// import { loadIntoDatabase, fetchSentiment } from "common/service";
+import api from "common/api";
 
 function ProductPage() {
+  const [productTitle, setProductTitle] = useState("");
+  //   const [thumbnail, setThumbnail] = useState("");
+  //   const [price, setPrice] = useState("");
+  //   const [reviews, setReviews] = useState("");
+  //   const [stars_stat, setStartStat] = useState("");
+  //   const [rating, setRating] = useState("");
+  //   const [reviewCount, setReviewCount] = useState("");
+  const [asin, setAsin] = useState("");
+  const [userID, setUserID] = useState("");
+  // eslint-disable-next-line
+  const [overallSentiment, setOverallSentiment] = useState("");
+  const reviewAccessData = {};
+  const location = useLocation();
 
-    const[productTitle, setProductTitle] = useState("");
-    const[thumbnail, setThumbnail] = useState("");
-    const[price,setPrice] = useState("");
-    const[reviews,setReviews] = useState("");
-    const[stars_stat,setStartStat] = useState("");
-    const[rating,setRating] = useState("");
-    const[reviewCount,setReviewCount] = useState("");
-    const[asin,setAsin] = useState("");
-    const[userID,setUserID] = useState("");
-    const[overallSentiment, setOverallSentiment] = useState("");
-    const reviewAccessData = {};
-    const location = useLocation();
-    
-    
+  useEffect(() => {
+    if (location.data) {
+      setUserID(location.data.userID);
+      setAsin(location.data.asin);
+      setProductTitle(location.data.title);
+      reviewAccessData["userID"] = userID;
+      reviewAccessData["asin"] = asin;
+    }
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(() => {
-        if (location.data) {
-            setUserID(location.data.userID);
-            setAsin(location.data.asin);
-            setProductTitle(location.data.title);
-            reviewAccessData["userID"] = userID;
-            reviewAccessData["asin"] = asin;
-        }  
-    });
-
-    const reviewAnalysis = async () => {
-        console.log(reviewAccessData);
-        axios.post('https://7jjweip03i.execute-api.us-east-1.amazonaws.com/default/reviewanalysis',reviewAccessData).then((result) => {
+  const reviewAnalysis = async () => {
+    console.log(reviewAccessData);
+    api
+      .post(
+        "https://7jjweip03i.execute-api.us-east-1.amazonaws.com/default/reviewanalysis",
+        reviewAccessData
+      )
+      .then((result) => {
         console.log(result.data.body);
-        demoFunc(result.data.body);
-        }).catch((err) => {
-            console.log(err);
-        }) 
-    }
+        // demoFunc(result.data.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    async function demoFunc(reviewData) {
-        const response = await loadIntoDatabase(reviewData).then((response) => {
-            console.log(response.data);
-            fetchSentiment(response.data).then((sentimentData) => {
-                setOverallSentiment('Review Sentiment for the Product is '+sentimentData.data.Items[0].overallSentiment);
-            });
-        });
-    } 
+  //   async function demoFunc(reviewData) {
+  //     const response = await loadIntoDatabase(reviewData).then((response) => {
+  //       console.log(response.data);
+  //       fetchSentiment(response.data).then((sentimentData) => {
+  //         setOverallSentiment(
+  //           "Review Sentiment for the Product is " +
+  //             sentimentData.data.Items[0].overallSentiment
+  //         );
+  //       });
+  //     });
+  //   }
 
-    async function fetchSentimentFunction(response) {
-        
-    }
+  //   async function fetchSentimentFunction(response) {}
 
-    return (
-        <div>
-            <div className="container">
-                <h2> {productTitle} </h2>
-            </div>
-            <button onClick={reviewAnalysis}>Review Analysis</button>
-            <h4>{overallSentiment}</h4>
-            <button>Rating visulization</button>
-        </div>
-    );
-
+  return (
+    <div>
+      <div className="container">
+        <h2> {productTitle} </h2>
+      </div>
+      <button onClick={reviewAnalysis}>Review Analysis</button>
+      <h4>{overallSentiment}</h4>
+      <button>Rating visulization</button>
+    </div>
+  );
 }
 
 export default ProductPage;
