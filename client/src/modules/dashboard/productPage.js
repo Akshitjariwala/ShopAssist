6e9069d8b6bad9225f7bf45c4a90449dd1741f18
Monisha J <mn504849@dal.ca>
2021-12-10 15:04:56 -0400

    Update productPage.js

--- client/src/modules/dashboard/productPage.js
+++ client/src/modules/dashboard/productPage.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation, useParams, useHistory } from "react-router-dom";
 import { isEmpty } from "lodash";
 
