c54d265f9cd563ca2be53c9102fca723af8ab1cf
Harsh Samirbhai Bhatt <hr559239@dal.ca>
2021-12-10 22:07:27 -0400

    Update config.js

--- client/src/common/config.js
+++ client/src/common/config.js
@@ -1,6 +1,6 @@
 export const config = {
   CLOUD_FUNCTION_URL: process.env.REACT_APP_CLOUD_FUNCTION_URL,
   SERVER_URL:
-    // "http://ec2-3-95-157-19.compute-1.amazonaws.com:8080" ||
+    "http://ec2-3-86-50-220.compute-1.amazonaws.com:8080" ||
     "http://localhost:8080",
 };
